e68ad844558816e48832e6184c370a038307215c
TomaszZayl <42319689+TomaszZayl@users.noreply.github.com>
2018-11-02 15:41:49 +0100

    Create ExcelFillerDispatchingDesk.java

---
+++ DDExcelFiller/ExcelFillerDispatchingDesk.java
@@ -0,0 +1,9 @@
+package dispaching.excel.writer.grabarczyk.tomasz;
+
+class ExcelFillerDispatchingDesk {
+
+    public static void main(String[] args) {
+        FieldListeners fieldListeners = new FieldListeners();
+        fieldListeners.fieldListeners();
+    }
+}
